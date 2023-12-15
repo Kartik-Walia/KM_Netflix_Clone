fef54e711d15125059e35fb25c9e1b3db7fa0e94
Kartik-Walia <itzkartikeds3@gmail.com>
2023-12-16 03:03:47 +0530

    API to trigger favorite Id

---
+++ pages/api/favorite.ts
@@ -0,0 +1,78 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { without } from "lodash";
+
+import prismadb from '@/lib/prismadb'
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        // Handling POST request (User favoriting a movie)
+        if (req.method === 'POST') {
+            const { currentUser } = await serverAuth(req);
+
+            const { movieId } = req.body;
+
+            // Find the movie suign the movieId
+            const existingMovie = await prismadb.movie.findUnique({
+                where: {
+                    id: movieId,
+                }
+            });
+
+            if (!existingMovie) {
+                throw new Error('Invalid ID');
+            }
+
+            // Update favorite movie of a user to it's Id
+            const user = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || '',
+                },
+                data: {
+                    favouriteIds: {
+                        push: movieId,
+                    }
+                }
+            });
+
+            return res.status(200).json(user);
+        }
+
+        // Handling DELETE request (User unfavoriting a movie)
+        if (req.method === 'DELETE') {
+            const { currentUser } = await serverAuth(req);
+
+            const { movieId } = req.body;
+
+            const existingMovie = await prismadb.movie.findUnique({
+                where: {
+                    id: movieId,
+                }
+            });
+
+            if(!existingMovie) {
+                throw new Error('Invalid ID');
+            }
+
+            // Update list of favorite ids
+            const updatedFavoriteIds = without(currentUser.favouriteIds, movieId);
+
+            const updatedUser = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || '',
+                },
+                data: {
+                    favouriteIds: updatedFavoriteIds,
+                }
+            });
+
+            return res.status(200).json(updatedUser);
+        }
+
+        // If we get a method which is neither POST or DELETE
+        return res.status(405).end();
+    } catch (error) {
+        console.log(error);
+        return res.status(400).end();
+    }
+}
