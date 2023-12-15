ea12200c96da510eb58c7c67b8cf975ad747117a
Kartik-Walia <itzkartikeds3@gmail.com>
2023-12-16 02:35:19 +0530

    Movie Cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import { getSession } from "next-auth/react"
 
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context:NextPageContext){
   const session = await getSession(context);
@@ -20,10 +22,16 @@ export async function getServerSideProps(context:NextPageContext){
   }
 }
 export default function Home() {
+  // Let's load our movies (Make sure default is just an empty array)
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar/>
       <Billboard/>
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies}/>
+      </div>
     </>
   )
 }
