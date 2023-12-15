bce795f461bc0a6d6514ed1807647b6689721b10
Kartik-Walia <itzkartikeds3@gmail.com>
2023-12-15 18:09:16 +0530

    Menu for Large devices

---
+++ components/Navbar.tsx
@@ -0,0 +1,59 @@
+import { useCallback, useState } from 'react';
+import { BsChevronDown } from 'react-icons/bs';
+
+import NavbarItem from "./Navbaritem";
+import MobileMenu from "./MobileMenu";
+
+const Navbar = () => {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+    const toggleMobileMenu = useCallback(() => {
+        setShowMobileMenu((current) => !current);
+    }, []);
+
+    return (
+        // i may relace this with w-screen
+        <nav className="w-full fixed z-40">     
+            <div 
+                className="
+                    px-4 
+                    md:px-16 
+                    py-6 
+                    flex 
+                    flex-row 
+                    items-center 
+                    transition 
+                    duration-500 
+                    bg-zinc-900 
+                    bg-opacity-90
+                "
+            >
+                <img className="h-4 lg:h-7" src="/images/logo.png" alt="Logo" />
+                <div 
+                    className="
+                        flex-row 
+                        ml-8 
+                        gap-7 
+                        hidden 
+                        lg:flex
+                    "
+                >
+                    <NavbarItem label="Home"/>
+                    <NavbarItem label="Series"/>
+                    <NavbarItem label="Films"/>
+                    <NavbarItem label="New & Popular"/>
+                    <NavbarItem label="My List"/>
+                    <NavbarItem label="Browse by languages"/>
+                </div>
+                {/* Create div for right side profile menu (gonna be visible on mobile screens) */}
+                <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+                    <p className="text-white text-sm">Browse</p>
+                    <BsChevronDown className="text-white transition"/>
+                    <MobileMenu visible={showMobileMenu}/>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
